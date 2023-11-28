34143cd6c0f1ce921efed864ee6388a691d6ce18
ZainBinTariq7 <zainbintariq7@gmail.com>
2023-11-28 12:21:03 +0500

    First commit

---
+++ delete-inline.php
@@ -0,0 +1,10 @@
+<?php
+    $stu_id = $_GET['id'];
+    include 'config.php';
+    $sql = "DELETE FROM student WHERE sid = '{$stu_id}'";
+    $result = mysqli_query($conn, $sql) or die("Query Unsuccessfull");
+
+    header("Location: index.php");
+
+    mysqli_close($conn);
+?>
